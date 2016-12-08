8962f1b4899c77e954d52def3da9f42f756feea1
Le Duc Tien <leductienttkt@gmail.com>
2016-12-08 16:01:23 +0700

    huhu

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
-  host = 'nameless-taiga-30814.herokuapp.com'
+  host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
   config.action_mailer.smtp_settings = {
